d6befdf9f07157c6c06e6ec251612a10d40f9cf7
Timhojin <ihavealreadylost@gmail.com.>
2024-08-16 17:40:55 +1000

    Nice and clean

--- script.js
+++ script.js
@@ -1,48 +1,28 @@
 const container = document.querySelector("#container");
-let gridsPerSide = 16;
 
-let row = [];
-let grid = [];
+function makeGrid(size) {
+    for(let i = 1; i <= size; i++) {
+        let row = document.createElement("div");
+        row.setAttribute("style", "flex: 1; display: flex;");
+        container.appendChild(row);
+        for(let j = 1; j <= size; j++) {
+            let square = document.createElement("div");
+            square.setAttribute("style", "flex: 1; border: black solid 1px;");
+            row.appendChild(square);
 
-for(let i = 1; i <= gridsPerSide; i++) {
-    row[i] = document.createElement("div");
-    row[i].setAttribute("style", "display: flex; flex: 1;");
-    container.appendChild(row[i]);
-
-    for(let j = 1; j <= gridsPerSide; j++) {
-        grid[j] = document.createElement("div");
-        grid[j].setAttribute("style", "flex: 1; border: 1px solid black;");
-        row[i].appendChild(grid[j]);
-        grid[i].addEventListener("mouseover", () => {
-            grid[i].style.background = "red";
-        });
-    }
-}
-
-let oldGridsPerSide = gridsPerSide;
-
-const changeGrid = document.querySelector("#change-grids");
-changeGrid.addEventListener("click", () => {
-    gridsPerSide = parseInt(prompt("How many grids do you want per side?\nPlease enter a number between 1 - 100"));
-    
-    for(let i = 1; i <= oldGridsPerSide; i++) {
-        container.removeChild(row[i]);
-    }
-
-    for(let i = 1; i <= gridsPerSide; i++) {
-        row[i] = document.createElement("div");
-        row[i].setAttribute("style", "display: flex; flex: 1;");
-        container.appendChild(row[i]);
-    
-        for(let j = 1; j <= gridsPerSide; j++) {
-            grid[j] = document.createElement("div");
-            grid[j].setAttribute("style", "flex: 1; border: 1px solid black;");
-            row[i].appendChild(grid[j]);
-            grid[i].addEventListener("mouseover", () => {
-                grid[i].style.background = "red";
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = "red";
             });
         }
     }
+}
 
-    oldGridsPerSide = gridsPerSide;
+makeGrid(16);
+
+const button = document.querySelector("#change-grids");
+button.addEventListener("click", () => {
+    let size = parseInt(prompt("How many squares do you want per side?\n Please insert a number between 1-100."));
+
+    container.innerHTML = "";
+    makeGrid(size);
 });
